84b89c38e751efd4ab8b874ad58a0ea240180683
Rice <112427358+ricefarmernft@users.noreply.github.com>
2026-01-08 13:38:08 +0700

    updated crypto section

--- app/components/crypto.js
+++ app/components/crypto.js
@@ -4,24 +4,27 @@ export default function Crypto() {
   return (
     <>
       <div className="text-lg md:text-2xl p-4 md:p-8 " id="crypto">
-        <p className="text-3xl md:text-6xl">Crypto</p>
-        <div className="pt-3 md:pt-6 flex flex-col md:flex-row gap-3 md:gap-6 items-center justify-between">
-          <div className="flex-1">
+        <p className="text-3xl md:text-6xl">Web 3</p>
+        <div className="pt-3 flex flex-col md:flex-row gap-3 md:gap-6 items-center justify-between">
+          <div className="flex-1 gap-4 flex flex-col ">
             <p>
-              I am extremely passionate about crypto and everything related to
-              web 3. Since I discovered Bitcoin in 2016, I have been fascinated
-              with blockchain technology and impressed with the growth of the
-              industry. I have experience working with blockchain data, smart
-              contracts, and NFTs. On{" "}
+              I've spent years building in Web3, sorting onchain data, writing
+              smart contracts, and leading NFT projects. I like it because
+              things move fast. You can ship something weird, put it in front of
+              people, and learn almost immediately if it's worth continuing.
+            </p>
+            <p>
+              Outside of code, I write and share insights on the space on{" "}
               <a
                 className="crypto-link"
-                href="https://twitter.com/RiceFarmerNFT"
+                href="https://x.com/RiceFarmerNFT"
                 target="_blank"
                 rel="noreferrer"
               >
                 Twitter
               </a>
-              , I am a content creator and have amassed over 25k+ followers.
+              , where I've grown an audience of 25k+ by breaking down trends and
+              products in a practical, builder-focused way.
             </p>
           </div>
           <div className="flex-1 flex flex-col gap-2 md:gap-4 items-center   ">
@@ -32,14 +35,6 @@ export default function Crypto() {
               width={200}
               height={200}
             ></Image>
-            {/* <a
-              className="crypto-link text-xl flex"
-              href="https://opensea.io/RiceFarmerVault"
-              target="_blank"
-              rel="noreferrer"
-            >
-              NFT Collection
-            </a> */}
           </div>
         </div>
       </div>
